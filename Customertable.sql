3a5611b36fc4499372f4e076ad6c17ac72820e06
unknown <ikrammohamudyusuf@gmail.com>
2024-12-05 16:39:06 +0200

    adding customers

---
+++ Customertable.sql
@@ -0,0 +1,12 @@
+-- INSERT INTO Customers (CustomerID, Name, Address) VALUES
+-- (1, 'Ahmed', 'Tunisia'),
+-- (2, 'Coulibaly', 'Senegal'),
+-- (3, 'Hasan', 'Egypt'),
+-- (4, 'Yasmine', 'Morocco'),
+-- (5, 'John', 'France'),
+-- (6, 'Fatima', 'Algeria');
+
+DELETE FROM Customers
+WHERE CustomerID = 3;
+
+
